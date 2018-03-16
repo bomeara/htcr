daf88e91ae6c9b2aa6e3f7b334e9f8f7f7d80895
Brian O'Meara <omeara.brian@gmail.com>
2018-03-15 22:47:40 -0400

    added executable creation

--- R/submit_file_create.R
+++ R/submit_file_create.R
@@ -5,7 +5,6 @@
 #'
 #' @param input Input files
 #' @param reps Number of replicates to do
-#' @param executable Executable file
 #' @param arguments Arguments to pass to executable
 #' @param log Log file
 #' @param output Output file (in addition to ones the script itself creates)
@@ -13,22 +12,25 @@
 #' @param universe HTCondor universe to use
 #' @param requirements  Requirements to pass as a requirements call
 #' @param other Other lines to add to the submit file (such as rank)
+#' @param executable Executable file name to make
 #' @param submit_file_name Name of the final submit file
 #' @examples
 #' \dontrun{
 #'  submit_file_create(input=c("birds.tre", "bird_traits.csv"), reps=3, executable="batch.R"))
 #'}
 #' @export
-submit_file_create <- function(input, reps=1, executable="run.sh", arguments=NULL, log='log.$(Process)', output='out.$(Process)', error='error.$(Process)', universe="vanilla", requirements=NULL, other=NULL, submit_file_name="Rjob.submit") {
+submit_file_create <- function(input, reps=1, arguments=NULL, log='log.$(Process)', output='out.$(Process)', error='error.$(Process)', universe="vanilla", requirements=NULL, other=NULL, executable="run.sh", submit_file_name="Rjob.submit") {
   final.file <- paste0("# Created using htcr at ", Sys.time(), "\n\n")
   final.file <- paste0(final.file, "\nUniverse = ", universe)
   if(!is.null(arguments)) {
     final.file <- paste0(final.file, "\nArguments = ", arguments)
   }
-  final.file <- paste0(final.file, "\nInput = ", paste(input, collapse=","))
+  final.file <- paste0(final.file, "\ntransfer_input_files = ", paste(input, collapse=",", sep=","))
   final.file <- paste0(final.file, "\nOutput = ", output)
   final.file <- paste0(final.file, "\nLog = ", log)
   final.file <- paste0(final.file, "\nError = ", error)
+  final.file <- paste0(final.file, "\nExecutable = ", executable)
+
   if(!is.null(requirements)) {
       final.file <- paste0(final.file, "\nRequirements = ", requirements)
   }
@@ -41,4 +43,17 @@ submit_file_create <- function(input, reps=1, executable="run.sh", arguments=NUL
     final.file <- paste0(final.file, " ", reps)
   }
   cat(final.file, file=submit_file_name)
+}
+
+#' Create the executable file to run R
+#'
+#' @description
+#' HTCondor needs a simple executable. This will make a file (with the name passed to \code{executable}) that contains a \code{Rscript} command followed by a script name (passed to this function with \code{rscript})
+#' @param rscript The R file to run
+#' @param executable Executable file name
+#' @export
+executable_file_create <- function(rscript = "batch.R", executable="run.sh") {
+  cat('#!/bin/bash', file=executable, append=FALSE)
+  cat("\n", file=executable, append=TRUE)
+  cat(paste0("Rscript ", rscript), file=executable, append=TRUE)
 }
